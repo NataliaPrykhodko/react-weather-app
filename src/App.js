8713abe63c0cb854b0348b521ac44a7d6484fe18
NataliaPrykhodko <prikhodko7964@gmail.com>
2023-01-30 20:42:39 +0200

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="#" target="_blank">
+          Natalia Prykhodko  
+        </a>{" "}
+        and is{" "}
+        <a href="https://github.com/NataliaPrykhodko/react-weather-app" >
+        open-sourced on GitHub </a>
+    </footer>
     </div>
   );
 }
