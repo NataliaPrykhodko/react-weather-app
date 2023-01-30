6fde90be874111ee17c39bbee6383349b23ff5f5
NataliaPrykhodko <prikhodko7964@gmail.com>
2023-01-30 21:07:26 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,11 +6,7 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <footer>
-        This project was coded by{" "}
-        <a href="#" target="_blank">
-          Natalia Prykhodko  
-        </a>{" "}
-        and is{" "}
+        This project was coded by Natalia Prykhodko and is{" "}
         <a href="https://github.com/NataliaPrykhodko/react-weather-app" >
         open-sourced on GitHub </a>
     </footer>
