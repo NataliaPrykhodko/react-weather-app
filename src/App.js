153f463e5bd2aa0ea6ccd9c46e041db1f90cd46f
NataliaPrykhodko <prikhodko7964@gmail.com>
2023-01-30 21:50:04 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+     <div className="container">
       <h1>Weather App</h1>
       <footer>
         This project was coded by Natalia Prykhodko and is{" "}
         <a href="https://github.com/NataliaPrykhodko/react-weather-app" >
         open-sourced on GitHub </a>
-    </footer>
+      </footer>
+     </div>
     </div>
   );
 }
