fc0af384967057488379cf4ff1efd8cffdc5a427
NataliaPrykhodko <prikhodko7964@gmail.com>
2023-02-04 15:44:02 +0200

    Change iconUrl

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
+import Weather from "./Weather";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
      <div className="container">
-      <h1>Weather App</h1>
-      <footer>
+        <h1>Weather App</h1>
+        <Weather defaultCity= "Berlin"/>
+      <footer class>
         This project was coded by Natalia Prykhodko and is{" "}
         <a href="https://github.com/NataliaPrykhodko/react-weather-app" >
         open-sourced on GitHub </a>
