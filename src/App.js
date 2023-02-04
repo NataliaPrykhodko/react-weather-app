851d561232cda7468ff191a7dad851316ddcae0a
NataliaPrykhodko <prikhodko7964@gmail.com>
2023-02-04 23:30:07 +0200

    change

--- src/App.js
+++ src/App.js
@@ -11,14 +11,13 @@ export default function App() {
       <footer class>
           This project was coded by{" "}
           <a href="https://www.linkedin.com/in/natalia-prykhodko-9a03b5227/?lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3B5rwBFBLGQqqzmmGSlyHP%2Fg%3D%3D"
-                    target="_blank" title="LinkedIn Profile">
+                    title="LinkedIn Profile">
             Natalia Prykhodko
           </a> and is{" "}
           <a href="https://github.com/NataliaPrykhodko/react-weather-app" >
             open-sourced on GitHub </a>{" "}
           and{" "}
-          <a href="https://zippy-druid-e27563.netlify.app/"
-                    target="_blank">
+          <a href="https://zippy-druid-e27563.netlify.app/">
           hosted on Netlify
           </a>
       </footer>
