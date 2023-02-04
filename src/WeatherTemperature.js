58f9b6b87fde52f90f37fba10e92ed7cdd208340
NataliaPrykhodko <prikhodko7964@gmail.com>
2023-02-04 18:26:10 +0200

    Convert temperature

---
+++ src/WeatherTemperature.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+export default function WeatherTemperature(props) {
+    const [unit, setUnit] = useState("celsius"); 
+   
+    function showToCelsius(event) {
+        event.preventDefault();
+        setUnit("celsius");
+    }
+
+    function showToFahrenheit(event) {
+        event.preventDefault();
+        setUnit("fahrenheit");
+    }
+    function fahrenheit() {
+        return (props.celsius * 9 / 5)+32;
+    }
+    
+    if (unit === "celsius") {
+        return (
+            <div className="WeatherTemperature">
+                <span className="temperature">{Math.round(props.celsius)}
+                </span>
+                <span className="unit">
+                    °C |{" "}
+                    <a href="/" onClick={showToFahrenheit}>
+                       °F 
+                    </a>
+                </span>
+            </div>
+        );
+    } else {
+       return (
+            <div className="WeatherTemperature">
+                <span className="temperature">{Math.round(fahrenheit)}
+                </span>
+                <span className="unit">
+                    <a href="/" onClick={showToCelsius}>
+                        °C
+                    </a>{" "}
+                    | °F
+                </span>
+            </div>
+        );
+    }
+} 
