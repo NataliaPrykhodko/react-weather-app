30cf1828b658098cf28612d420a965dcd12d98d2
NataliaPrykhodko <prikhodko7964@gmail.com>
2023-02-04 22:57:32 +0200

    changtd Footer

--- src/App.js
+++ src/App.js
@@ -6,11 +6,21 @@ export default function App() {
   return (
     <div className="App">
      <div className="container">
-        <Weather defaultCity= "paris"/>
+        <h1>Weather App</h1>
+        <Weather defaultCity= "Berlin"/>
       <footer class>
-        This project was coded by Natalia Prykhodko and is{" "}
-        <a href="https://github.com/NataliaPrykhodko/react-weather-app" >
-        open-sourced on GitHub </a>
+          This project was coded by{" "}
+          <a href="https://www.linkedin.com/in/natalia-prykhodko-9a03b5227/?lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3B5rwBFBLGQqqzmmGSlyHP%2Fg%3D%3D"
+                    target="_blank" title="LinkedIn Profile">
+            Natalia Prykhodko
+          </a> and is{" "}
+          <a href="https://github.com/NataliaPrykhodko/react-weather-app" >
+            open-sourced on GitHub </a>{" "}
+          and{" "}
+          <a href="https://zippy-druid-e27563.netlify.app/"
+                    target="_blank">
+          hosted on Netlify
+          </a>
       </footer>
      </div>
     </div>
